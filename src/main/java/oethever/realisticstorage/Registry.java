408a747d3d2698ecca856730bccb1c17482876ff
Oethever <66912376+Oethever@users.noreply.github.com>
2022-05-14 23:47:07 +0200

    Use voxel to disable culling

--- src/main/java/oethever/realisticstorage/Registry.java
+++ src/main/java/oethever/realisticstorage/Registry.java
@@ -1,22 +1,21 @@
 package oethever.realisticstorage;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import oethever.realisticstorage.block.BlockPallet;
 
 public class Registry {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RealisticStorage.MOD_ID);
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RealisticStorage.MOD_ID);
 
-    public Registry() {
+    public static void register() {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 
--- src/main/java/oethever/realisticstorage/block/BlockPallet.java
+++ src/main/java/oethever/realisticstorage/block/BlockPallet.java
@@ -1,19 +1,57 @@
 package oethever.realisticstorage.block;
 
-import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BlockPallet extends Block {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    private static final Properties PROPERTIES = Properties.of(Material.WOOD);
+    private static final VoxelShape SHAPE = Shapes.or(
+            Block.box( 1,  0,  0,  4,  3, 16),
+            Block.box(12,  0,  0, 15,  3, 16),
+            Block.box( 0,  3,  0, 16,  4,  4),
+            Block.box( 0,  3,  6, 16,  4, 10),
+            Block.box( 0,  3, 12, 16,  4, 16),
+
+            Block.box( 1,  4,  0,  4,  7, 16),
+            Block.box(12,  4,  0, 15,  7, 16),
+            Block.box( 0,  7,  0, 16,  8,  4),
+            Block.box( 0,  7,  6, 16,  8, 10),
+            Block.box( 0,  7, 12, 16,  8, 16),
+
+            Block.box( 1,  8,  0,  4, 11, 16),
+            Block.box(12,  8,  0, 15, 11, 16),
+            Block.box( 0, 11,  0, 16, 12,  4),
+            Block.box( 0, 11,  6, 16, 12, 10),
+            Block.box( 0, 11, 12, 16, 12, 16),
+
+            Block.box( 1, 12,  0,  4, 15, 16),
+            Block.box(12, 12,  0, 15, 15, 16),
+            Block.box( 0, 15,  0, 16, 16,  4),
+            Block.box( 0, 15,  6, 16, 16, 10),
+            Block.box( 0, 15, 12, 16, 16, 16)
+    );
+
+    private static final VoxelShape FLIPPED_SHAPE = SHAPE.toAabbs().stream().map(
+        aabb -> Shapes.create(new AABB(
+                aabb.minZ, aabb.minY, aabb.minX,
+                aabb.maxZ, aabb.maxY, aabb.maxX
+        ))
+    ).reduce(Shapes::or).get();
 
     public BlockPallet() {
-        super(Properties.of(Material.WOOD));
+        super(PROPERTIES);
     }
 
     @Override
@@ -24,5 +62,16 @@ public class BlockPallet extends Block {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        switch (state.getValue(FACING)) {
+            case NORTH:
+            case SOUTH:
+                return SHAPE;
+            default:
+                return FLIPPED_SHAPE;
+        }
     }
 }
