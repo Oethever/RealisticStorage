51ebedb71baf50c8f451bfc51c7d75dd7561dd33
Oethever <66912376+Oethever@users.noreply.github.com>
2022-05-14 18:46:52 +0200

    Add the Pallet (no interaction yet)

--- src/main/java/oethever/realisticstorage/RealisticStorage.java
+++ src/main/java/oethever/realisticstorage/RealisticStorage.java
@@ -2,35 +2,26 @@ package oethever.realisticstorage;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("realisticstorage")
+@Mod(RealisticStorage.MOD_ID)
 public class RealisticStorage
 {
-    // Directly reference a log4j logger.
+    public static final String MOD_ID = "realisticstorage";
     private static final Logger LOGGER = LogManager.getLogger();
-    ContainerGuard containerGuard = new ContainerGuard(LOGGER);
+    private static final ContainerGuard containerGuard = new ContainerGuard(LOGGER);
+    private final Registry registry;
 
     public RealisticStorage() {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(containerGuard);
+        this.registry = new Registry();
         Config.init();
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-
-        }
     }
 }
---
+++ src/main/java/oethever/realisticstorage/block/BlockPallet.java
@@ -0,0 +1,28 @@
+package oethever.realisticstorage.block;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.Material;
+
+public class BlockPallet extends Block {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    public BlockPallet() {
+        super(Properties.of(Material.WOOD));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
