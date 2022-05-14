c5188ae353405fb3e32c9b90e7d10a17a62d9a52
Oethever <66912376+Oethever@users.noreply.github.com>
2022-05-14 23:47:07 +0200

    Use voxel to disable culling

--- src/main/java/oethever/realisticstorage/Config.java
+++ src/main/java/oethever/realisticstorage/Config.java
@@ -5,7 +5,6 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
--- src/main/java/oethever/realisticstorage/RealisticStorage.java
+++ src/main/java/oethever/realisticstorage/RealisticStorage.java
@@ -1,27 +1,27 @@
 package oethever.realisticstorage;
 
-import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(RealisticStorage.MOD_ID)
-public class RealisticStorage
-{
+@Mod.EventBusSubscriber(
+        modid = RealisticStorage.MOD_ID,
+        bus = Mod.EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+)
+public class RealisticStorage {
     public static final String MOD_ID = "realisticstorage";
     private static final Logger LOGGER = LogManager.getLogger();
     private static final ContainerGuard containerGuard = new ContainerGuard(LOGGER);
-    private final Registry registry;
 
     public RealisticStorage() {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(containerGuard);
-        this.registry = new Registry();
+        Registry.register();
         Config.init();
     }
 }
