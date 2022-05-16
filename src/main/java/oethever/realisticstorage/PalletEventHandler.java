4ffde1becbc0f17b01b03faa8918c5f9e6913e31
Oethever <https://github.com/Oethever/Primitive-Technology.git>
2022-05-16 15:00:02 +0200

    use noOcclusion rather than voxel, implement pallet behaviour

--- src/main/java/oethever/realisticstorage/PalletEventHandler.java
+++ src/main/java/oethever/realisticstorage/PalletEventHandler.java
@@ -1,15 +1,18 @@
 package oethever.realisticstorage;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import oethever.realisticstorage.block.BlockPallet;
+import oethever.realisticstorage.block.Util;
 
 @Mod.EventBusSubscriber(
         modid = RealisticStorage.MOD_ID,
@@ -17,32 +20,21 @@ import oethever.realisticstorage.block.BlockPallet;
         value = Dist.CLIENT
 )
 public class PalletEventHandler {
-    private static final float FAST_BREAK_SPEED = 40;
     private static final int AREA_HEIGHT = 8;
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void breakSpeedEvent(PlayerEvent.BreakSpeed event) {
-        LevelAccessor world = event.getEntity().level;
+    public static void breakSpeedEvent(PlayerInteractEvent.LeftClickBlock event) {
+        Level world = event.getEntity().level;
         if (world.isClientSide()) {
             return;
         }
-        if (isAbovePallet(world, event.getPos())) {
-            event.setNewSpeed(FAST_BREAK_SPEED);
-            event.setCanceled(false);
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void breakEvent(BlockEvent.BreakEvent event) {
-        LevelAccessor world = event.getWorld();
-        if (world.isClientSide()) {
-            return;
-        }
-        if (isAbovePallet(world, event.getPos())) {
-            event.setCanceled(false);
-            if (event.hasResult()) {
-                event.setResult(Event.Result.ALLOW);
-            }
+        BlockPos pos = event.getPos();
+        if (isAbovePallet(world, pos)) {
+            event.setUseBlock(Event.Result.DENY);
+            event.setUseItem(Event.Result.DENY);
+            event.setCanceled(true);
+            Util.spawnItem(world, getSilkTouchDrop(world.getBlockState(pos)), pos);
+            world.removeBlock(pos, false);
         }
     }
 
@@ -50,18 +42,16 @@ public class PalletEventHandler {
         if (pos == null) {
             return false;
         }
-        RealisticStorage.LOGGER.info("Looking for pallet from " + pos.toShortString());
-        for (int i = 1; i < AREA_HEIGHT; ++i) {
+        for (int i = 1; i < AREA_HEIGHT + 1; ++i) {
             BlockPos lowerPos = pos.below(i);
             if (world.getBlockState(lowerPos).getBlock() instanceof BlockPallet) {
-                RealisticStorage.LOGGER.info("found pallet at " + lowerPos.toShortString());
                 return true;
             }
         }
         return false;
     }
 
-    //private static ItemStack getSilkTouchDrop(BlockState state) {
-    //    return new ItemStack(state.getBlock().asItem());
-    //}
+    private static ItemStack getSilkTouchDrop(BlockState state) {
+        return new ItemStack(state.getBlock().asItem());
+    }
 }
