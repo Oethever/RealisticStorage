10c5bfc9c37c4e63e37420e895a9a6e3232c38ed
Oethever <66912376+Oethever@users.noreply.github.com>
2022-05-17 14:26:37 +0200

    Version 1.1: added support for Inspirations and Realistic Torches

--- src/main/java/oethever/realisticstorage/RealisticStorage.java
+++ src/main/java/oethever/realisticstorage/RealisticStorage.java
@@ -37,7 +37,7 @@ import java.util.regex.PatternSyntaxException;
 public class RealisticStorage {
     public static final String MODID = "realisticstorage";
     public static final String NAME = "Realistic Storage";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @Mod.Instance(MODID)
     public static RealisticStorage INSTANCE;
