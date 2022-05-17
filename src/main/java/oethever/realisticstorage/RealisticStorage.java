11e56249db3757afeef424956b3657fb2894486c
Oethever <66912376+Oethever@users.noreply.github.com>
2022-05-17 14:26:51 +0200

    Add silk touch to pallet

--- src/main/java/oethever/realisticstorage/RealisticStorage.java
+++ src/main/java/oethever/realisticstorage/RealisticStorage.java
@@ -1,22 +1,7 @@
 package oethever.realisticstorage;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import oethever.realisticstorage.blocks.BlockPallet;
-import oethever.realisticstorage.blocks.TileEntityPallet;
 import oethever.realisticstorage.containerguard.ContainerGuard;
 import org.apache.logging.log4j.Logger;
 
@@ -34,12 +19,13 @@ public class RealisticStorage {
     public static RealisticStorage INSTANCE;
 
     private static Logger logger;
-    private ContainerGuard containerGuard;
+    private ContainerGuard containerGuard = new ContainerGuard();
+    private ServerEventHandler serverEventHandler = new ServerEventHandler();
 
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
-        containerGuard = new ContainerGuard(logger);
+        containerGuard.setLogger(logger);
     }
 
     public void updateConfig() {
--- src/main/java/oethever/realisticstorage/blocks/TileEntityPallet.java
+++ src/main/java/oethever/realisticstorage/blocks/TileEntityPallet.java
@@ -1,58 +1,12 @@
 package oethever.realisticstorage.blocks;
 
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.fml.common.eventhandler.Event;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import oethever.realisticstorage.Utils;
+import oethever.realisticstorage.RealisticStorage;
+import oethever.realisticstorage.ServerEventHandler;
 
 public class TileEntityPallet extends TileEntity {
 
-    private final float fastBreakSpeed = 50;
-    private final float areaHeight = 8;
-
     public TileEntityPallet() {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void getBreakSpeed(PlayerEvent.BreakSpeed event) {
-        if (isInArea(event.getPos())) {
-            event.setNewSpeed(fastBreakSpeed);
-            event.setCanceled(false);
-            event.setResult(Event.Result.ALLOW);
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void doPlayerHarvestCheck(PlayerEvent.HarvestCheck event) {
-        if (isInArea(Utils.getTargetedBlock(event.getEntityPlayer()))) {
-            event.setCanHarvest(true);
-            event.setCanceled(false);
-            event.setResult(Event.Result.ALLOW);
-        }
-
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void breakBlock(BlockEvent.BreakEvent event) {
-        if (isInArea(event.getPos())) {
-            event.setCanceled(false);
-            event.setResult(Event.Result.ALLOW);
-        }
-    }
-
-    private boolean isInArea(BlockPos pos) {
-        return pos != null &&
-                getPos() != null &&
-                pos.getX() == getPos().getX() &&
-                pos.getZ() == getPos().getZ() &&
-                pos.getY() > getPos().getY() &&
-                pos.getY() <= getPos().getY() + areaHeight;
+        ServerEventHandler.addPallet(this);
     }
 }
