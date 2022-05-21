f4b0e605b50f4f68d93a5963642a79a09ce84503
Oethever <66912376+Oethever@users.noreply.github.com>
2022-05-21 17:46:40 +0200

    Don't reimplement GuiComponent.fill

--- src/main/java/oethever/realisticstorage/handlers/DrawEventHandler.java
+++ src/main/java/oethever/realisticstorage/handlers/DrawEventHandler.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Matrix4f;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.world.entity.player.Inventory;
@@ -31,13 +32,11 @@ public class DrawEventHandler {
     private static final int SLOT_SIZE = 16;
     private static final int SLOTS_PER_ROW = 9;
     public static void init() {
-
         OverlayRegistry.registerOverlayAbove(
                 ForgeIngameGui.HOTBAR_ELEMENT,
                 "Realistic Storage Locked Slots",
                 (gui, poseStack, partialTicks, screenWidth, screenHeight) -> drawLockedSlots(poseStack)
         );
-
     }
 
     private static void drawLockedSlots(PoseStack poseStack) {
@@ -58,7 +57,7 @@ public class DrawEventHandler {
         for (int i = Config.CONFIG.getSlotLimit(); i < SLOTS_PER_ROW; ++i) {
             int left = hotbarLeft + hotbarSlotGap * i;
             int right = left + SLOT_SIZE;
-            fill(poseStack, left, hotbarTop, right, hotbarBottom, GREY_COLOR);
+            GuiComponent.fill(poseStack, left, hotbarTop, right, hotbarBottom, GREY_COLOR);
         }
     }
 
@@ -75,44 +74,10 @@ public class DrawEventHandler {
                 if (!player.isCreative() && SlotEventHandler.isLockedSlot(slot.getSlotIndex())) {
                     PoseStack poseStack = new PoseStack();
                     poseStack.pushPose();
-                    fill(poseStack, slot.x, slot.y, slot.x + SLOT_SIZE, slot.y + SLOT_SIZE, GREY_COLOR);
+                    GuiComponent.fill(poseStack, slot.x, slot.y, slot.x + SLOT_SIZE, slot.y + SLOT_SIZE, GREY_COLOR);
                     poseStack.popPose();
                 }
             }
         }
-    }
-
-    private static void fill(PoseStack poseStack, int xMin, int yMin, int xMax, int yMax, int color) {
-        if (xMax < xMin) {
-            int i = xMax;
-            xMax = xMin;
-            xMin = i;
-        }
-
-        if (yMax < yMin) {
-            int j = yMax;
-            yMax = yMin;
-            yMin = j;
-        }
-
-        Lighting.setupForEntityInInventory();
-        RenderSystem.enableBlend();
-        RenderSystem.disableTexture();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.setShader(GameRenderer::getPositionColorShader);
-
-        Tesselator tesselator = Tesselator.getInstance();
-        BufferBuilder bufferbuilder = tesselator.getBuilder();
-        bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-        Matrix4f transform = poseStack.last().pose();
-        bufferbuilder.vertex(transform, (float)xMax, (float)yMin, 0.0F).color(color).endVertex();
-        bufferbuilder.vertex(transform, (float)xMin, (float)yMin, 0.0F).color(color).endVertex();
-        bufferbuilder.vertex(transform, (float)xMin, (float)yMax, 0.0F).color(color).endVertex();
-        bufferbuilder.vertex(transform, (float)xMax, (float)yMax, 0.0F).color(color).endVertex();
-        tesselator.end();
-
-        RenderSystem.enableTexture();
-        RenderSystem.disableBlend();
-        Lighting.setupFor3DItems();
     }
 }
